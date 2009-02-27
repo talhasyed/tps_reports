4afd2b074a16df8a97072a36024914677da7b478
talha <talha.syed@kaboose.com>
2009-02-27 15:45:09 -0500

    [CHANGED] config file to add git repo;

--- config.rb
+++ config.rb
@@ -3,13 +3,17 @@ REPOS = {
     :repo => "svn+ssh://talha@svn.bubbleshare.com/svn/admin",
     :vcs => :svn
   },
-  :csp => {
-    :repo => "svn+ssh://talha@svn.bubbleshare.com/svn/csp/",
-    :vcs => :svn
+  :local => {
+    :repo => "svn.bubbleshare.com:/git/local",
+    :vcs => :git
   }
+  # :csp => {
+  #   :repo => "svn+ssh://talha@svn.bubbleshare.com/svn/csp/",
+  #   :vcs => :svn
+  # }# ,
 }
 
 USERS = ['talha', 'Talha Syed']
 
-BEGIN_DATE = '2008-1-1'
-END_DATE = '2008-12-30'
+BEGIN_DATE = '2008-09-07'
+END_DATE = '2008-09-10'
