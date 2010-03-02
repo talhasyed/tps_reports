0234f8e584ea12e11cc2905964290ff75752d576
talha <talha.syed@gmail.com>
2010-03-02 11:28:16 -0500

    [REMOVED] extra comment;

--- vc_adapters/git.rb
+++ vc_adapters/git.rb
@@ -15,7 +15,7 @@ module TPS
       unfiltered_commits = Commit.find_all(repo, 'master', :max_count => 600)
       
       unfiltered_commits.each do |uc| 
-        date = Date.parse uc.to_hash['authored_date'] #uc.date.to_s
+        date = Date.parse uc.to_hash['authored_date']
         author = uc.author.to_s
         should_keep = (@users.include?(author) && @date_range.include?(date))
         messages = uc.message.to_s.split("\n")
